ba38eb0498e92e76c8b21a35a23f6311d243c67d
Rodrigo Quezada <roderik.ikana@gmail.com>
2022-07-12 00:28:32 +0000

    feat: add scrapper

---
+++ scrapper/exec.py
@@ -0,0 +1,18 @@
+"""
+Scrap the data from the UK government website
+using beautifulsoup4 and requests
+"""
+import boto3
+
+def main():
+    """
+    Entry function
+    """
+    sts = boto3.client('sts')
+    response = sts.get_caller_identity()
+
+    print(response)
+
+if __name__ == '__main__':
+    main()
+    exit(0)
